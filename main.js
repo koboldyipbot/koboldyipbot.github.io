d0cb8e02c5215bd2e7d0412d6fe7c124674ecc0f
Crom <james.vogon@gmail.com>
2022-11-05 11:10:09 -0700

    sans

---
+++ main.js
@@ -0,0 +1,185 @@
+// Define configuration options
+const opts = {
+  identity: {
+    username: config.twitchUsername,
+    password: config.twitchOAuthPassword
+  },
+  channels: config.twitchChannels
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+// client.on('raw_message', (message) => {
+//   console.log(message.raw);
+//   console.log(message);
+// });
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.on('cheer', cheer_yip);
+
+// Connect to Twitch:
+client.connect();
+
+function poll_channel_points() {
+
+}
+
+function postLink() {
+  client.say("kobold_wyx", "https://youtu.be/2vrAc_c9RRI");
+  setTimeout(postLink, 600000);
+}
+
+function doHelp(channel, context) {
+  var user = context["display-name"];
+  var userYips = fetchUserYips(user);
+  client.say(channel, "@" + user + ": You have " + userYips.yips + " yips! To use them, run `!yip <number of yips> <milliseconds between yips>`");
+}
+
+function hello() {
+  var audio = new Audio('sounds/hello.mp3');
+  audio.loop = false;
+  audio.play();
+}
+
+// Called every time a message comes in
+function onMessageHandler (channel, context, msg, self) {
+  if (self) { return; }
+
+  // console.log(context);
+  // console.log(msg);
+  var user = context["display-name"];
+  updateYips(user);
+  raffleAddEntryToday(user);
+
+  if (!msg.startsWith('!')) { return; }
+
+  // Remove whitespace from chat message
+  const command = msg.trim();
+  console.log(command);
+
+  // If the command is known, let's execute it
+  var commandArr = command.split(/\s+/);
+
+  if (commandArr[0] === "!yiphelp") {
+    doHelp(channel, context);
+  } else if (commandArr[0] === "!yipsong") {
+      var song = commandArr[1];
+      if (song === "mario") {
+        marioYip();
+      } else if (song === "girl") {
+        girlInTheTowerYip();
+      } else if (song === "charge") {
+        chargeYip();
+      } else if (song === "sans") {
+        sansYip();
+      } else if (song === "custom") {
+        if (commandArr.length > 18) {
+          client.say(channel, "Only 16 notes max!");
+        } else {
+          var songString = commandArr.slice(2).join(" ");
+          var song = detokenizeYipSong(songString);
+          var valid = true;
+          if (song) {
+            playSong(song);
+          } else {
+            client.say(channel, "Invalid custom song! You can pass a midi pitch number or that number and a note length inside square brackets, ie. [75, 400] will play C5 for .4 seconds");
+          }
+        }
+      } else {
+        client.say(channel, "Current songs: custom, mario, girl, charge");
+      }
+  } else if (commandArr[0] === '!yip') {
+
+    var args = command.split(/ +/, 4);
+    if (args[1] === "give" && config.mods.includes(user)) {
+      var user = args[2];
+      var yips = parseInt(args[3]);
+
+      updateYips(user, yips);
+
+    } else {
+      var yips = parseInt(args[1]);
+      var msPerYip = parseInt(args[2]);
+      if ((isNaN(yips) || isNaN(msPerYip)) && !state.helpInCooldown) {
+        doHelp(channel, context);
+        return;
+      }
+
+      if (msPerYip < config.minimumYipGapMilliseconds) {
+        client.say(channel, "@" + user + ": The minimum gap between yips in milliseconds is " + config.minimumYipGapMilliseconds + "!");
+        return;
+      }
+
+      var userYips = fetchUserYips(user);
+      if (userYips.yips <= 0) {
+        var user = context["display-name"];
+        client.say(channel, "@" + user + ": You only have " + userYips.yips + " yips!");
+        return;
+      }
+      
+      yip(yips, msPerYip);
+      updateYips(user, -yips);
+    }
+  } else if (commandArr[0] === "!hello") {
+    var isMod = config.mods.includes(user);
+    if (isMod && command.slice(7, 10) === "off") {
+      state.helloEnabled = false;
+    } else if (isMod && command.slice(7, 10) === "on") {
+      state.helloEnabled = true;
+    } else if (state.helloEnabled || isMod) {
+      hello();
+    }
+  } else if (commandArr[0] === "!raffle") {
+    var isMod = config.mods.includes(user);
+    var cmd = commandArr[1];
+    if (isMod) {
+      // meta commands
+      if (cmd === "enable") {
+        raffleSetIsEnabled(true);
+      } else if (cmd === "disable") {
+        raffleSetIsEnabled(false);
+      } else if (cmd === "raffleOn") {
+        raffleSetAcceptEntries(true);
+      } else if (cmd === "raffleOff") {
+        raffleSetAcceptEntries(false);
+      }
+      // commands
+      else if (raffleIsEnabled) {
+        if (cmd === "draw") {
+          raffleDrawCommand(client, channel);
+        } else if (cmd === "add" && commandArr[2] && commandArr[3]) {
+          var day = commandArr[2];
+          var user = commandArr[3];
+          raffleAddEntry(day, user);
+        } else if (cmd === "clear") {
+          raffleClearEntries();
+        } else if (cmd === "checkEntries") {
+          var user = commandArr[2];
+          raffleAdminQueryCommand(client, channel, user);
+        } else if (cmd === "debug") {
+          raffleDebug(client, channel);
+        }
+      }
+    } else if (raffleIsEnabled) {
+      raffleQueryCommand(client, channel, user);
+    }
+  }
+}
+
+// Function called when the "dice" command is issued
+// function rollDice () {
+//   const sides = 6;
+//   return Math.floor(Math.random() * sides) + 1;
+// }
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+  // postLink();
+}
+
+$.when( $.ready ).then(function() {
+    $(document).click(yip);
+});
---
+++ yip.js
@@ -0,0 +1,233 @@
+var baseYip = new Audio("sounds/yip.mp3");
+
+function fetchUserYips(user) {
+  if (!(user in state.userYips)) {
+    let yips = localStorage.getItem(user);
+    if (yips == null) {
+        yips = 0;
+    } else {
+        yips = parseInt(yips);
+    }
+    state.userYips[user] = {
+      yips: yips,
+      commandCooldown: false,
+      lastTimestamp: new Date()
+    };    
+  }
+
+  return state.userYips[user];
+}
+
+function updateYips(user, extraYips) {
+  var userYips = fetchUserYips(user);
+  if (config.mods.includes(user)) {
+    userYips.yips = 99999999999999;
+  } else {
+    var newTimestamp = new Date();
+    var seconds = Math.floor(getSecondsDiff(newTimestamp, userYips.lastTimestamp) / config.secondsPerYip);
+    if (seconds > 0) {
+      userYips.lastTimestamp = new Date();
+      userYips.yips = Math.min(100, userYips.yips + seconds + (extraYips || 0));
+      localStorage.setItem(user, userYips.yips);
+    } else {
+        userYips.yips = Math.min(100, userYips.yips + (extraYips || 0));
+    }
+    console.log("h" + userYips.yips);
+  }
+}
+
+function reset_yip() {
+    $("#yip").attr("src", config.image1);
+    state.isYipping = false;
+}
+
+function marioYip() {
+    playSong([
+        65, 65, 65, 61, 65, 68, 56
+    ]);
+}
+
+function girlInTheTowerYip() {
+    playSong([
+        59.6,
+        61.3,
+        62.5, 
+        [62.5, 600], 
+        [61.3, 300], 
+        61.3, 
+        59.6, 
+        [62.5, 600], 
+        [61.3, 300], 
+        61.3,
+        64.2, 
+        [64.2, 600], 
+        [62.5, 300], 
+        62.5,
+        61.3, 
+        59.6
+    ]);
+}
+
+function chargeYip() {
+    playSong([
+        [55, 200],
+        [60, 200],
+        [64, 200],
+        [67, 400],
+        [64, 150],
+        67
+    ]);
+}
+
+function sansYip() {
+    playSong([
+        [62, 200], [62, 200], 74, 69
+    ]);
+}
+
+function detokenizeYipSong(song) {
+    // return null if invalid song
+    var splitted = song.replaceAll('[', '[ ').replaceAll(']', ' ]').replaceAll(",", "").split(/\s+/);
+    var song = [];
+    var index = 0;
+    while (index < splitted.length) {
+        if (splitted[index] === "[") {
+            // pitch and length
+            var pitch = Number(splitted[index+1]);
+            var length = Number(splitted[index+2]);
+            if (!pitch || !length) {
+                return null;
+            }
+            song.push([pitch, length]);
+            if (splitted[index+3] != "]") {
+                return null;
+            }
+            index += 4;
+        } else {
+            // just pitch
+            var pitch = Number(splitted[index]);
+            if (!pitch) {
+                return null;
+            }
+            song.push(pitch);
+            index += 1;
+        }
+    }
+    return song;
+}
+
+function playSong(song, forceSong, index) {
+    if (forceSong === undefined && state.isSinging) {
+        console.log(":(")
+        return;
+    }
+    if (index === undefined) {
+        index = 0;
+    }
+    state.isSinging = true;
+    var a = baseYip.cloneNode(true);
+    a.mozPreservesPitch = false;
+    a.webkitPreservesPitch = false;
+    a.preservesPitch = false;
+
+    a.volume = 0.05;
+
+    var midiPitch;
+    var lengthInMillis;
+    if (Array.isArray(song[index])) {
+        midiPitch = song[index][0];
+        lengthInMillis = song[index][1];
+    } else {
+        midiPitch = song[index];
+        lengthInMillis = config.defaultYipSongNoteLength;
+    }
+
+    a.playbackRate = Math.pow(2, (1+((midiPitch-70)/12)));
+    a.play();
+    animate_yip();
+    if (index < song.length-1) {
+        setTimeout(playSong, lengthInMillis, song, true, index+1);
+    } else {
+        state.isSinging = false;
+    }
+}
+
+function cheer_yip(channel, context, msg, self) {
+    yip(client, target, yipCount, msPerYip);
+}
+
+function animate_yip() {
+    var size = Math.floor(Math.random() * 18) + 6;
+    var span = $('<span class="yip">yip</span>');
+    span.css({
+        "font-size": size + "px",
+        left: config.yipOffsetLeft,
+        top: config.yipOffsetTop
+    });
+    $("#yip_box").append(span);
+    last_anim = span;
+
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    var target_x = Math.floor(width * (Math.random() * 2.4 - 1.6));
+    var target_y = Math.floor(-height);
+
+    var dir = Math.random() > 0.5 ? -1 : 1;
+
+    var iters = 10;
+    var wiggle_multiplier = 0.1;
+    var animation_time = 400;
+
+    for (var i = 1 ; i <= iters; i++) {
+        var next_x_line = i * target_x / iters;
+        var next_x_wiggle = wiggle_multiplier * width * dir;
+        var next_y_line = i * target_y / iters + config.yipOffsetTop;
+
+        var doneFunc = function(){};
+        if (i === iters) {
+            doneFunc = function () {
+                span.remove();
+            };
+        }
+        last_anim = last_anim.animate(
+            {
+                left: Math.floor(config.yipOffsetLeft + next_x_line + next_x_wiggle),
+                top: Math.floor(config.yipOffsetTop + next_y_line)
+            },
+            {
+                duration: animation_time,
+                specialEasing: {
+                    left: "swing",
+                    top: "linear"
+                },
+                done: doneFunc
+            }
+        );
+        dir = dir * -1;
+    }
+    var nextDate = new Date();
+    if (!state.isYipping && getMillisecondsDiff(state.lastYipDate, nextDate) > config.minimumYipGapMilliseconds * 2) {
+        state.isYipping = true;
+        state.lastYipDate = nextDate;
+
+        $("#yip").attr("src", config.image2);
+        setTimeout(reset_yip, config.minimumYipGapMilliseconds);
+    }
+}
+
+function yip(yipCount, msPerYip) {
+    var audio = baseYip.cloneNode(true);
+    audio.volume = 0.05;
+    audio.loop = false;
+    audio.mozPreservesPitch = false;
+    audio.webkitPreservesPitch = false;
+    audio.preservesPitch = false;
+    audio.playbackRate = 0.8 + Math.random()*0.8
+    audio.play();
+    animate_yip();
+
+    if (yipCount-1 > 0) {
+        setTimeout(yip, msPerYip, yipCount-1, msPerYip);
+    }
+}
