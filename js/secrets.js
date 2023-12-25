a98135d29b6e94a5a1a4ec977f377f96164f8432
Ames <james.vogon@gmail.com>
2023-12-24 20:11:43 -0800

    add blank secrets.js

---
+++ js/secrets.js
@@ -0,0 +1,12 @@
+// temp fix bc yip.monster is broken
+
+const secrets = {
+    twitchOAuthPassword: "", // Official OAuth token generator: https://twitchapps.com/tmi/
+    twitchChannelPoints: {
+        username: "",
+        channelID: "",
+        refreshToken: "",
+        clientID: "",
+        clientSecret: ""
+    },
+};  
